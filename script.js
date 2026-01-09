008cf83b748ddb649a70de81586285dba0154f21
Alexis Bissuel <alexisbissuel.dev@gmail.com>
2026-01-09 17:39:20 +0100

    Service Workers PWA

--- script.js
+++ script.js
@@ -525,6 +525,9 @@ renderMonth(currentYear, currentMonth);
 // ----------------------------
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js");
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then(() => console.log("SW OK"))
+      .catch((err) => console.error("SW ERROR", err));
   });
 }
